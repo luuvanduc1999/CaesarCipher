838945c19fa9d39ebef4671d06f835ef504edba3
Lưu Văn Đức <50072440+luuvanduc1999@users.noreply.github.com>
2020-03-24 20:35:25 +0700

    Update multiplicative_cipher.js

--- multiplicative_cipher.js
+++ multiplicative_cipher.js
@@ -16,22 +16,21 @@ function findInverse(x, z)
 }
 
 
-function decryptMultiplicative(cipherText, a, b)
+function decryptMultiplicative(cipherText, key)
 {
     var alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
     cTextChar=cipherText.toLowerCase().split("");
     plainText="";
 
-    if (GCD(a,26)!=1) return("Cannot decrypt!");
+    if (GCD(key,26)!=1) return("Cannot decrypt!");
     
-    var da=findInverse(a,26);
-    var db=b;
+    var invKey=findInverse(key,26);
 
     for (var i=0; i<cipherText.length; i++)
         if (cTextChar[i].match("[a-z]|[A-Z]"))
             {
                 cTextChar[i]=alphabet.indexOf(cTextChar[i]);
-                plainText+=(alphabet[(da*cTextChar[i]).mod(26)]);
+                plainText+=(alphabet[(invKey*cTextChar[i]).mod(26)]);
             }
         else    plainText+=cipherText[i];
       
@@ -41,7 +40,7 @@ function decryptMultiplicative(cipherText, a, b)
 
 
 
-function encryptMultiplicative(plainText, a, b)
+function encryptMultiplicative(plainText, key)
 {
     var alphabet=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
     pTextChar=plainText.toUpperCase().split("");
@@ -50,7 +49,7 @@ function encryptMultiplicative(plainText, a, b)
         if (pTextChar[i].match("[a-z]|[A-Z]"))
             {
                 pTextChar[i]=alphabet.indexOf(pTextChar[i]);
-                cipherText+=(alphabet[(pTextChar[i]*a).mod(26)]);
+                cipherText+=(alphabet[(pTextChar[i]*key).mod(26)]);
             }
         else    cipherText+=plainText[i];
     return (cipherText);
